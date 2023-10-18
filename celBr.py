f6ad0140009733493350c5dfe27921af038b9a60
lukasfonks <ls_fonseca@hotmail.com>
2023-10-18 15:14:16 -0300

    teste validação numero telefone

---
+++ celBr.py
@@ -0,0 +1,26 @@
+import re
+
+
+def validar_telefone(numero):
+    # Remover caracteres não numéricos
+    numero = re.sub(r'\D', '', numero)
+
+    # Verificar se o número tem 11 dígitos (incluindo o DDD)
+    if len(numero) != 11:
+        return None
+
+    # Formatar o número: (XX) 9XXXX-XXXX
+    numero_formatado = f'({numero[:2]}) {numero[2:7]}-{numero[7:]}'
+
+    return numero_formatado
+
+
+# Exemplo de uso
+# telefone = input("Digite o número de telefone brasileiro: ")
+
+# numero_formatado = validar_telefone(telefone)
+
+# if numero_formatado:
+#     print(f"Número formatado: {numero_formatado}")
+# else:
+#     print("Número de telefone inválido.")
--- tests/test_diversitech.py
+++ tests/test_diversitech.py
@@ -1,15 +1,11 @@
-import sys
-sys.path.append('C:\Users\ls_fo\PycharmProjects\diversitech\tests')
-
-
-from telefonesBr import TelefonesBr
+import celBr
 
 class TestClass:
-    def test_quando_telefone_recebe_7133812599_deve_retornar_7133812599_validado_e_formatado(self):
-        entrada = '7133812599' #Given-Contexto
-        esperado = '(71)3381-2599'
+    def test_quando_telefone_recebe_71981296060_deve_retornar_formatado(self):
+        entrada = '71981296060' #Given-Contexto
+        esperado = '(71) 98129-6060'
 
-        telefone_teste_resultado = TelefonesBr(entrada) #When-Ação
+        teste_resultado = celBr.validar_telefone(entrada) #When-Ação
         
-        assert telefone_teste_resultado == esperado #Then-Desfecho
+        assert teste_resultado == esperado #Then-Desfecho
 
